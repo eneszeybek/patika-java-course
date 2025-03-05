db87d73f3b5e53a5f75d9feb0435682c615bfe2e
eneszeybek <eneszeybek@gmail.com>
2025-03-05 20:50:39 +0300

    .

--- src/main/java/Week1/ReverseTriangle.java
+++ src/main/java/Week1/ReverseTriangle.java
@@ -2,20 +2,27 @@ package Week1;
 
 import java.util.Scanner;
 
+//Java ile basamak sayısının kullanıcıdan alınan ve döngüler kullanılarak, yıldızlar(*) ile ekrana ters üçgen çizen programı yazın.
+
 public class ReverseTriangle {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
+        //Kullanıcıdan üçgenin taban uzunluğunu belirtecek bir değer istiyoruz ve bu değeri num değişkenine atıyoruz.
         System.out.println("Bir sayı giriniz:");
         int num = scanner.nextInt();
-        int i, j;
 
-        for (i=num ; i>0 ; i--){
-            for (j=0; j<i; j++){
+        //ilk for döngümüz num değerinden başlayıp 1'e kadar azalarak kaç satır * yazdırılacağını belirleyecek
+        for (int i=num ; i>0 ; i--){
+
+            //ikinci for döngümüz, mevcut satıra i'nin o anki değerine kadar * yazdıracak
+            for (int j=0; j<i; j++){
                 System.out.print("*");
             }
-            System.out.print("\n");
+
+            //satırda olması gereken kadar * yazıldıktan sonra alt satıra geçeceğiz.
+            System.out.println();
         }
     }
 }
