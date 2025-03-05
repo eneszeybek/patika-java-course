8fb5017e6aa6c3f1cd191b98f50a9c82e91d5939
eneszeybek <eneszeybek@gmail.com>
2025-03-05 19:01:01 +0300

    .

--- src/main/java/Week1/ChineseZodiac.java
+++ src/main/java/Week1/ChineseZodiac.java
@@ -4,10 +4,19 @@ import java.util.Scanner;
 
 public class ChineseZodiac {
     public static void main(String[] args) {
+
+        // Kullanıcıdan input alınacağını koda bildiriyoruz.
         Scanner scanner = new Scanner(System.in);
+
+        /*Kullanıcıdan doğum yılı bilgisini alarak bu değeri
+        yearOfBirth tamsayı değişkenine atıyoruz. */
 
         System.out.println("Doğum yılınızı giriniz: ");
         int yearOfBirth = scanner.nextInt();
+
+        /* Switch-case döngüsü ile; doğum yılının 12 ile bölümünden kalanı
+        hesaplayıp, değerin döngü içindeki Çin Zodyağı burç karşılığı ile eşleştirip,
+        bu ismi chineseZodiac olarak tanımladığımız string değişkenine atıyoruz. */
 
         String chineseZodiac = switch (yearOfBirth % 12){
             case 0 -> "Maymun";
@@ -22,9 +31,10 @@ public class ChineseZodiac {
             case 9 -> "Yılan";
             case 10 -> "At";
             case 11 -> "Koyun";
-            default -> "";
+            default -> "Geçersiz değer girdiniz";
         };
 
-        System.out.println("Çin Zodyağı Burcunuz:" + chineseZodiac);
+        //Doğum yılına karşılık gelen Çiz Zodyağı burcunu ekrana yazdırıyoruz.
+        System.out.println("Çin Zodyağı Burcunuz: " + chineseZodiac);
     }
 }
