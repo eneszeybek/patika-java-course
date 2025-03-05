ce9018c6995921179d7a09226c563e1708c71c22
eneszeybek <eneszeybek@gmail.com>
2025-03-05 20:27:16 +0300

    .

--- src/main/java/Week1/Average3_4.java
+++ src/main/java/Week1/Average3_4.java
@@ -2,24 +2,40 @@ package Week1;
 
 import java.util.Scanner;
 
+/*Java döngüler ile 0'dan girilen sayıya kadar olan sayılardan 3 ve 4'e tam bölünen sayıların ortalamasını hesaplayan programı yazınız.*/
+
 public class Average3_4 {
     public static void main(String[] args) {
 
+        //Koda kullanıcıdan input girileceğini bildiriyoruz.
         Scanner scanner = new Scanner(System.in);
 
+        //Kullanıcıdan input isteyip aldığımız değeri number integer değişkenine atıyoruz.
         System.out.println("Bir sayı giriniz:");
-        int num = scanner.nextInt();
+        int number = scanner.nextInt();
 
-        int sum = 0;
+        /*Ortalama hesaplamak için toplam değere ve bu değerin kaç sayıdan oluştuğuna ihtiyacımız var.
+        Sayıların toplamını sum integer değişkeninde, kaç adet sayı olduğunu ise counter integer değişkeninde tutacağız.
+        Sonucun küsüratlı gösterilebilmesi için değerlerden en az birini double tipi değişken olarak tanımlıyoruz.
+        İki değişkenin de başlangıç değeri 0 olmalı. */
+
+        double sum = 0;
         int counter = 0;
-        int i = 1;
-        while (i<=num){
+
+        //for döngüsü içinde 1'den kullanıcının girdiği değere kadar olan sayıların koşulu sağlayıp sağlamadığını kontrol edeceğiz.
+
+        for (int i=1; i<=number; i++){
             if(i%3 == 0 && i%4 == 0){
+
+                //i değeri koşulu her sağladığında sum değeri sum+i olarak güncellenecek. Döngü sonuna kadar koşulu sağlayan tüm sayıların toplamını bu şekilde tutmuş olacağız.
                 sum += i;
+
+                //Koşul her sağlandığında counter sayacımız 1 artacak.
                 counter++;
             }
-            i++;
         }
-        System.out.println(num + "'ye kadar olan sayılardan 3 ve 4'e tam bölünenlerin ortalaması = " + (sum/counter) + "'dır");
+
+        //Ekrana "Girilen değer + açıklama + Ortalama(sum/counter)" bilgisini yazdırıyoruz.
+        System.out.println(number + "'ye kadar olan sayılardan 3 ve 4'e tam bölünenlerin ortalaması = " + (sum/counter) + "'dır");
     }
 }
