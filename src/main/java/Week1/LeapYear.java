881d0c1f5dcf72e39608fa666c3627c05ffdbe0a
eneszeybek <eneszeybek@gmail.com>
2025-03-05 19:43:23 +0300

    .

--- src/main/java/Week1/LeapYear.java
+++ src/main/java/Week1/LeapYear.java
@@ -4,21 +4,33 @@ import java.util.Scanner;
 
 public class LeapYear {
     public static void main(String[] args) {
+
+        //Koda kullanıcıdan input alınacağını bildiriyoruz.
         Scanner scanner = new Scanner(System.in);
+
+        //Kullanıcıdan bir değer girmesini istiyoruz ve bu değeri year integer değişkenine atıyoruz.
 
         System.out.println("Lütfen bir yıl giriniz: ");
         int year = scanner.nextInt();
 
-        if (year%400 == 0){
-            System.out.println(year + " artık yıldır.");
-        }
-        else if (year%100 == 0){
-            System.out.println(year + " artık yıl değildir.");
-        }
-        else if (year%4 == 0){
+        //year değişkeninin artık yıl olup olmadığını if döngüsü ile kontrol ediyoruz.
+
+        if (year%4 == 0 && (year%100 != 0 || year%400 == 0)){
+
+        /*Koşulumuz sayının 4'e bölünebilmesi ve 100'ün katları ise 400'ün de katı olmasıdır.
+        Değerimiz 100'ün katı ve 400'ün katı değilse if(1 && (0 || 0)) -> if(1 && 0) -> if(0)
+        şeklinde çalışıp koşulu sağlamayacaktır.
+        Değerimiz 400'ün katı ise if(1 && (0 || 1)) -> if(1 && 1) -> if(1)
+        şeklinde çalışıp koşulu sağlayacaktır. */
+
+            //Koşul sağlanıyorsa ekrana "year değeri artık yıldır." yazdırıyoruz.
+
             System.out.println(year + " artık yıldır.");
         }
         else {
+
+            //Koşul sağlanmıyorsa ekrana "year değeri artık yıl değildir." yazdırıyoruz.
+
             System.out.println(year + " artık yıl değildir.");
         }
     }
