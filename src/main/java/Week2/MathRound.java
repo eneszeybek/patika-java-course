5d37be1743e71ef1b077877c800fe9b83634bf3a
eneszeybek <eneszeybek@gmail.com>
2025-03-13 22:06:39 +0300

    MathRound committed

---
+++ src/main/java/Week2/MathRound.java
@@ -0,0 +1,17 @@
+package Week2;
+
+import java.util.Scanner;
+
+public class MathRound {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lütfen bir sayı giriniz: ");
+        double number = scanner.nextDouble();
+
+        System.out.println("Yukarı yuvarlanmış hali: " + (int)Math.ceil(number));
+        System.out.println("Aşağı yuvarlanmış hali: " + (int)Math.floor(number));
+        System.out.println("En yakın tamsayı: " + Math.round(number));
+
+    }
+}
