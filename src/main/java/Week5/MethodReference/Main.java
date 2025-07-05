5e5922454671a56ff0c8ff237f0b4ca2b8b24d1e
eneszeybek <eneszeybek@gmail.com>
2025-07-05 21:54:23 +0300

    .

---
+++ src/main/java/Week5/MethodReference/Main.java
@@ -0,0 +1,20 @@
+package Week5.MethodReference;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> names = new ArrayList<>();
+
+        names.add("Ahmet");
+        names.add("Ayşe");
+        names.add("Mehmet");
+        names.add("Zeynep");
+
+        // Method reference kullanarak liste elemanlarını yazdırma
+        names.forEach(System.out::println);
+    }
+}
